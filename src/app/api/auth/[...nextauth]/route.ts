f38ef6f92c07a2946ed73d6f0259fa7846e244e3
wen <wen.l@dottomesh.com>
2025-01-18 16:03:09 +0800

    deploy: no need export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,1 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-export const authOptions = {
-  // 配置认证提供者
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-    }),
-  ],
-  secret: process.env.AUTH_SECRET,
-}
-
-export const { handlers: { GET, POST }, auth } = NextAuth(authOptions)
+export { GET, POST } from '@/lib/auth'
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,19 @@
-import { auth } from "@/app/api/auth/[...nextauth]/route";
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 import { fetchChatRoomPermissions } from "@/lib/data"
+
+const authOptions = {
+  // 配置认证提供者
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+}
+
+export const { handlers: { GET, POST }, auth } = NextAuth(authOptions)
 
 export async function checkRoomAuth(roomId: number) {
     const session = await auth();
